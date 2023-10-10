fbfeb5e0df04305618443bdf0bdfb1fe61c3c132
theforager <0xforager@gmail.com>
2023-10-10 17:22:17 -0500

    Create deploy script

--- circuits/HelloNoir/script/Deploy.s.sol
+++ circuits/HelloNoir/script/Deploy.s.sol
@@ -2,11 +2,11 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
+import "../circuits/contract/plonk_vk.sol";
 
-contract CounterScript is Script {
-    function setUp() public {}
-
+contract DeployScript is Script {
     function run() public {
         vm.broadcast();
+        UltraVerifier v = new UltraVerifier();
     }
 }
