5b5d72a37da4146e7fce124bf27df5782c09cc08
theforager <0xforager@gmail.com>
2023-10-21 20:10:47 -0500

    Test deployment

---
+++ circuits/Sudoku/script/Deploy.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+import "../circuits/contract/Sudoku/plonk_vk.sol";
+
+contract DeployScript is Script {
+    function run() public {
+        vm.broadcast();
+        UltraVerifier v = new UltraVerifier();
+    }
+}
